3de95dad94306d79f2002793664cf9ed29f45b4c
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-05-09 02:26:20 -0500

    Another push Done twice wrong third time the charm

--- if-google-was-a-librarian/client/src/App.js
+++ if-google-was-a-librarian/client/src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import SavedBooks from "./pages/SavedBooks";
+import Search from "./pages/Search";
+import NoMatch from "./pages/NoMatch";
+import './App.css';
+import logo from "./book.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
+      <p className="App-intro">
+       To get started, search for a book below!
+      </p>
+      <div className="container.fluid">
+        <Router>
+          <Navbar />
+          <Title />
+          <Switch>
+            <Route exact path="/" component={SavedBooks}/>
+            <Route exact path="/books" component={SavedBooks}/>
+            <Route exact path="/search" component={Search}/>
+            <Route component={NoMatch}/>
+            </Switch>
+            </Router>
+            </div>  
+      
+   </div>
     );
   }
 }
